df9defcb40619d21f6854eb3f28a8a864c269ca5
Étienne Weil <etienne.weil@af83.com>
2017-02-22 14:19:40 +0100

    first approach of general search quick and dirty

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,7 +12,12 @@ class StaticPagesController < ApplicationController
   end
 
   def search
-    #code
+    @people = params[:q].nil? ? [] : Person.where(" lastname LIKE  ? or firstname LIKE  ? ", sqlPerc(params[:q]), sqlPerc(params[:q])).paginate(page: params[:page])
+    @companies = params[:q].nil? ? [] : Company.where(" company_name LIKE ? ", sqlPerc(params[:q])).paginate(page: params[:page])
+    @jobs = params[:q].nil? ? [] : Job.where(" job_title LIKE ? ", sqlPerc(params[:q])).paginate(page: params[:page])
+    #@search_companies = Person.find(params[:q]) #.paginate(page: params[:page])
+    #@search_companies = Mission.find(params[:q]) #.paginate(page: params[:page])
+    render 'static_pages/search_results'
   end
 
 end
