4d0d1b144702ebec3c0ef2ec675e96308411bdd5
Étienne Weil <etienne.weil@af83.com>
2017-02-23 16:16:53 +0100

    light changes in search results page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,10 +14,15 @@ class StaticPagesController < ApplicationController
   def search
     @people = params[:q].nil? ? [] : Person.where(" lastname LIKE  ? or firstname LIKE  ? ", sqlPerc(params[:q]), sqlPerc(params[:q])).paginate(page: params[:page])
     @companies = params[:q].nil? ? [] : Company.where(" company_name LIKE ? ", sqlPerc(params[:q])).paginate(page: params[:page])
-    @jobs = params[:q].nil? ? [] : Job.where(" job_title LIKE ? ", sqlPerc(params[:q])).paginate(page: params[:page])
-    #@search_companies = Person.find(params[:q]) #.paginate(page: params[:page])
-    #@search_companies = Mission.find(params[:q]) #.paginate(page: params[:page])
+    @jobs = params[:q].nil? ? [] : Job.where(" job_title LIKE ? ", sqlPerc(params[:q])).paginate(page: params[:page]).joins(:person).includes(:person)
+
     render 'static_pages/search_results'
+  end
+
+  private
+  def search_params
+    params.permit(:q)
+    #code
   end
 
 end
