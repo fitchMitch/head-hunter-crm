941512d4836faa10a1fa41600ba9bdb3c67dd9db
Étienne Weil <etienne.weil@af83.com>
2017-02-24 17:53:42 +0100

    passed tests are ok

--- test/controllers/missions_controller_test.rb
+++ test/controllers/missions_controller_test.rb
@@ -2,31 +2,30 @@ require 'test_helper'
 
 class MissionsControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @mission = missions(:one)
+    @user       = users(:michael)
+    @mission    = missions(:one)
   end
 
   test "should get edit" do
+    log_in_as(@user)
     get edit_mission_path(@mission)
     assert_response :success
   end
 
   test "should get new" do
+    log_in_as(@user)
     get new_mission_path
     assert_response :success
   end
 
-
-  test "should get update" do
-    get update_mission_path(@mission)
-    assert_response :success
-  end
-
   test "should get show" do
+    log_in_as(@user)
     get mission_path(@mission)
     assert_response :success
   end
 
   test "should get index" do
+    log_in_as(@user)
     get missions_path
     assert_response :success
   end
