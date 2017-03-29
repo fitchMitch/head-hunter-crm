84ab5cf88e1118ad64532d5afb1aaf1f93eb9277
Étienne Weil <etienne.weil@af83.com>
2017-03-29 18:35:01 +0200

    better job_unemployment sig

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,4 +3,19 @@ module PeopleHelper
     %w{M. Mme Mlle}
   end
 
+  def delay_bar(num)
+    num = (num>0) ? num : -num
+    step = 3*365 / 12
+    i=12
+    (0..12).each do  |j|
+      i = 12 - j.to_i
+      break if(num > i * step)
+    end
+    str = '<div class="row"> <div class="col-xs-'
+    str += i.to_s
+    str += ' grav"></div> </div>'
+    str += i.to_s
+    str.html_safe
+  end
+
 end
