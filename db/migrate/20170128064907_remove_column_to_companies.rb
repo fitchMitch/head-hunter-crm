0490a9bb41666eedc2398ca463d42fcc0e98f061
Etienne <weil.etienne@hotmail.fr>
2017-05-28 22:54:04 +0200

    slight db parameters change and migration change

--- db/migrate/20170128064907_remove_column_to_companies.rb
+++ db/migrate/20170128064907_remove_column_to_companies.rb
@@ -1,5 +1,5 @@
 class RemoveColumnToCompanies < ActiveRecord::Migration[5.0]
   def change
-    remove_column :companies, :job_id, :string
+    # remove_column :companies, :job_id, :string
   end
 end
