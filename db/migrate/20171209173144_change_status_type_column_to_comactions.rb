19839648ef32883d16e8ef9cf03a2235c9e6d2e5
Etienne <weil.etienne@hotmail.fr>
2017-12-27 13:42:40 +0100

    migration debug default values in comactions enums

--- db/migrate/20171209173144_change_status_type_column_to_comactions.rb
+++ db/migrate/20171209173144_change_status_type_column_to_comactions.rb
@@ -2,7 +2,7 @@ class ChangeStatusTypeColumnToComactions < ActiveRecord::Migration[5.0]
   def change
     remove_column :comactions, :status, :string
     add_column :comactions,:status, :integer
-    remove_column :comactions, :action_type, :string
-    add_column :comactions, :action_type, :integer
+    remove_column :comactions, :action_type, :string, default: 0
+    add_column :comactions, :action_type, :integer, default: 0
   end
 end
