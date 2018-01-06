e33eb5bf02011f7c61bcca098f153e79a43b9710
Etienne <weil.etienne@hotmail.fr>
2018-01-06 16:27:54 +0100

    some cleaning

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,10 +4,10 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = 'Votre compte est activé'
+      flash[:success] = I18n.t("user.authentication.activation")
       redirect_to user
     else
-      flash[:danger] = 'Le lien d\'activation est invalide'
+      flash[:danger] = I18n.t("user.authentication.link_no_working")
       redirect_to root_url
     end
   end
