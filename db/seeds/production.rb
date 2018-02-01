39798ab5bedf291ac0c7cdfe25aa156fa7147988
Etienne <weil.etienne@hotmail.fr>
2018-02-01 11:38:44 +0100

    publishing

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'as-duration'
+# require 'as-duration'
 #-----------------
 # Common
 #-----------------
@@ -14,10 +7,12 @@ updated_at=           Time.zone.now
 #-----------------
 # Users
 #-----------------
-User.create!(name:                   'Etienne WEIL',
-    email:                 "weil.etienne@hotmail.fr",
-    password:              "123456",
-    password_confirmation: "123456",
-    admin:                  true,
-    activated: true,
-    activated_at: Time.zone.now)
+User.create!(
+  name:                  'Etienne WEIL',
+  email:                 'weil.etienne@hotmail.fr',
+  password:              '123456',
+  password_confirmation: '123456',
+  admin:                  true,
+  activated:              true,
+  activated_at:           Time.zone.now
+)
